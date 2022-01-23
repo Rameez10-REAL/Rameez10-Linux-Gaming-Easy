57e77e203291dfcc8a001b65a7238ba0516a3e45
Rameez10 <Rameezmajid10@outlook.com>
2022-01-23 08:26:11 +0000

    Add files via upload
    Fixed version

---
+++ Gaming.sh
@@ -0,0 +1,22 @@
+sudo apt update
+sudo apt upgrade
+sudo dpkg --add-architecture i386
+sudo apt install steam
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' -y
+sudo add-apt-repository ppa:lutris-team/lutris -y
+sudo apt update
+sudo apt-get install --install-recommends winehq-staging -y
+sudo apt-get install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 -y
+sudo apt-get install lutris -y
+ulimit -Hn
+apt install meson libsystemd-dev pkg-config ninja-build git libdbus-1-dev libinih-dev dbus-user-session -y
+git clone https://github.com/FeralInteractive/gamemode.git
+cd gamemode
+git checkout 1.5.1 # omit to build the master branch
+./bootstrap.sh
+pip install protonup
+echo "export PATH=$PATH:~/.local/bin" >> .bashrc
+source .bashrc
+protonup
