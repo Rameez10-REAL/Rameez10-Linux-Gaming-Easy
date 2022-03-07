7a17c0d712c69eb110555fc513097f73d9fef3e5
Rameez10 <Rameezmajid10@outlook.com>
2022-03-07 18:30:44 +0000

    Update Gaming.sh
    Improves the code

--- Gaming.sh
+++ Gaming.sh
@@ -1,23 +1,23 @@
 sudo apt update
-sudo apt upgrade
-sudo dpkg --add-architecture i386
-sudo apt install steam
+sudo apt upgrade #Updates your system
+sudo dpkg --add-architecture i386 #adds 32bit support
+sudo apt install steam #Installs Steam
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
-sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' -y
-sudo add-apt-repository ppa:lutris-team/lutris -y
-sudo apt update
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' -y #Set up wine
+sudo add-apt-repository ppa:lutris-team/lutris -y #Install Lutris
+sudo apt-get install lutris -y
 sudo apt-get install --install-recommends winehq-staging -y
 sudo apt-get install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 -y
-sudo apt-get install lutris -y
 sudo apt install pip
-ulimit -Hn
-apt install meson libsystemd-dev pkg-config ninja-build git libdbus-1-dev libinih-dev dbus-user-session -y
+sudo apt install meson libsystemd-dev pkg-config ninja-build git libdbus-1-dev libinih-dev dbus-user-session -y
 git clone https://github.com/FeralInteractive/gamemode.git
 cd gamemode
-git checkout 1.5.1 # omit to build the master branch
+git checkout 1.5.1 
 ./bootstrap.sh
 pip install protonup
 echo "export PATH=$PATH:~/.local/bin" >> .bashrc
 source .bashrc
 protonup
+#How to install graphics drivers
+xdg-open https://github.com/lutris/docs/blob/master/InstallingDrivers.md
